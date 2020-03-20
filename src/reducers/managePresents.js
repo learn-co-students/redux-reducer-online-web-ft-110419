a18ed33660fc812b8a37ad2ae5be8e1e30151261
github username <github email address>
2020-03-19 21:40:31 -0400

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -4,9 +4,7 @@ export function managePresents(state = {
   switch(action.type) {
 
     case "INCREASE":
-      return Object.assign({}, state, {
-        numberOfPresents: state.numberOfPresents + 1
-      });
+      return {...state, numberOfPresents: state.numberOfPresents + 1};
 
     default:
       return state;
