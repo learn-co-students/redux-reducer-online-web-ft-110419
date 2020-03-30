c348838ca5e762a4e51258a74cdafc88c8ef3de7
cejimenez0123 <christianjimenez0123@gmail.com>
2020-03-30 16:12:42 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,18 @@
-export function manageFriends(state, action){
+export function manageFriends(prevState, action){
+    switch(action.type){
+        case 'ADD_FRIEND' :
+            
+        return (Object.assign({},prevState,{friends: [...prevState.friends,action.friend]}))
+        
+        case 'REMOVE_FRIEND':
+        return Object.assign({}, prevState, {
+            friends: prevState.friends.filter(friend => friend.id !== action.id)
+          });
+        default:
+            return prevState
+        }
+        
+    
 }
+
+// (Object.assign({},prevState, {friends: [ ...prevState.friends.slice(0,index),prevState.friends.slice(index+1)]}))
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,10 @@
 export function managePresents(state, action){
+    switch(action.type){
+        case "INCREASE":
+            return {numberOfPresents: state.numberOfPresents + 1}
+        case "DECREASE":
+            return {numberOfPresents: state.managePresents - 1}
+        default:
+                return state
+    }
 }
